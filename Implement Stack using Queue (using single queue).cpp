63274cbba01daef0bf7d5bdb399fea677dd23910
Ankita Krushna Jamdade <66841978+AnkitaKrushnaJamdade@users.noreply.github.com>
2022-07-18 00:09:10 +0530

    Create Implement Stack using Queue (using single queue).cpp

---
+++ Implement Stack using Queue (using single queue).cpp
@@ -0,0 +1,62 @@
+#include <queue>
+
+class Stack {
+	// Define the data members.
+    
+   public:
+    queue<int> st;
+   
+    
+    /*----------------- Public Functions of Stack -----------------*/
+
+    int getSize() {
+        // Implement the getSize() function.
+        return st.size();
+    }
+
+    bool isEmpty() {
+        // Implement the isEmpty() function.
+        if(!st.size())
+            return true;
+        return false;
+    }
+
+    void push(int element) {
+        // Implement the push() function.
+        if(st.empty()){
+            st.push(element);
+            return;
+        }
+        
+        queue<int> t;
+        
+        while(!st.empty()){
+            t.push(st.front());
+            st.pop();
+        }
+        
+        st.push(element);
+        
+        while(!t.empty()){
+            st.push(t.front());
+            t.pop();
+        }        
+    }
+
+    int pop() {
+        // Implement the pop() function.
+        if(!st.size())
+            return -1;
+        int dele = st.front();
+        st.pop();
+//         currTop--;
+        return dele;
+    }
+
+    int top() {
+        // Implement the top() function.
+         if(!st.size())
+            return -1;
+        return st.front();
+    }
+};
